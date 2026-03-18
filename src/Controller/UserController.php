86867aabc0da27690870f622ad49768c5a5785a8
David Horès <hores.d@gmail.com>
2026-03-18 20:50:11 +0100

    Create route and create method in UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,65 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
+    public function __construct(
+        private readonly SerializerInterface $serializer,
+    ) {}
+
+    #[Route('/api/users', name: 'users_create', methods: ['POST'])]
+    public function create(
+        Request $request,
+        UserRepository $userRepository,
+        ValidatorInterface $validator,
+        UserPasswordHasherInterface $hasher,
+        EntityManagerInterface $manager
+    ): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
+        $existingUser = $userRepository->findOneBy(['email' => $user->getEmail()]);
+
+        if ($existingUser) {
+            return new JsonResponse(
+                json_encode(['error' => 'Cette adresse email est déjà utilisée.']),
+                JsonResponse::HTTP_CONFLICT,
+                [],
+                true
+            );
+        }
+
+        $errors = $validator->validate($user);
+
+        if ($errors->count() > 0) {
+            return new JsonResponse(
+                $this->serializer->serialize($errors, 'json'),
+                JsonResponse::HTTP_BAD_REQUEST,
+                [],
+                true
+            );
+        }
+
+        $hashedPassword = $hasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hashedPassword);
+        $manager->persist($user);
+        $manager->flush();
+
+        $context = SerializationContext::create()->setGroups(['user_detail']);
+        $jsonUser = $this->serializer->serialize($user, 'json', $context);
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, [], true);	
     }
 }
