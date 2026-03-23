dd37022241541c6bb65a63087ee3f59f350bab0a
David Horès <hores.d@gmail.com>
2026-03-23 16:39:47 +0100

    Create route and index method in WeatherController

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -7,6 +7,7 @@ use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class WeatherController extends AbstractController
@@ -14,6 +15,23 @@ final class WeatherController extends AbstractController
     public function __construct(
         private readonly SerializerInterface $serializer,
     ) {}
+
+    #[Route('/api/weather', name: 'weather_index', methods: ['GET'])]
+    public function index(WeatherService $weatherService): JsonResponse
+    {
+        $user = $this->getUser();
+
+        if (!$user || !$user->getCity()) {
+            throw new HttpException(Response::HTTP_BAD_REQUEST, 'La ville de votre compte est introuvable.');
+        }
+
+        $city = $user->getCity();
+        $weather = $weatherService->getWeather($city);
+
+        $jsonWeather = $this->serializer->serialize($weather, 'json');
+
+        return new JsonResponse($jsonWeather, Response::HTTP_OK, [], true); 
+    }
 
     #[Route('/api/weather/{city}', name: 'weather_show', methods: ['GET'], requirements: ['city' => '[a-zA-ZÀ-ÿ\' \-]+'])]
     public function show(WeatherService $weatherService, string $city): JsonResponse
